22c023896186feac1c8578b9bc30a17176581579
Felix A. Croes <felix@dworkin.nl>
2015-01-23 21:42:04 +0100

    Memory allocation and errors.

---
+++ jit/jit.c
@@ -0,0 +1,36 @@
+# include <stdlib.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include "jit.h"
+
+/*
+ * NAME:	fatal()
+ * DESCRIPTION:	fatal error
+ */
+void fatal(char *format, ...)
+{
+    va_list argp;
+    char buf[1024];
+
+    va_start(argp, format);
+    vsprintf(buf, format, argp);
+    va_end(argp);
+    printf("Fatal error: %s\n", buf);
+
+    abort();
+}
+
+/*
+ * NAME:	jit->alloc()
+ * DESCRIPTION:	allocate memory in JIT compiler
+ */
+void *jit_alloc(size_t size)
+{
+    void *mem;
+
+    mem = malloc(size);
+    if (mem == NULL) {
+	fatal("cannot allocate memory");
+    }
+    return mem;
+}
