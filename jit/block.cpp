062a8ab9bd93c20f473be54653f74e24bb744526
Felix A. Croes <felix@dworkin.nl>
2018-05-16 17:34:32 +0200

    Block => C++

--- jit/block.cpp
+++ jit/block.cpp
@@ -1,5 +1,6 @@
 # include <stdlib.h>
 # include <stdint.h>
+# include <new>
 extern "C" {
 # include "lpc_ext.h"
 }
@@ -9,31 +10,28 @@ extern "C" {
 # include "jitcomp.h"
 
 
-/*
- * NAME:	Block->new()
- * DESCRIPTION:	allocate new block
- */
-static Block *block_new(Code *first, Code *last, CodeSize size)
+Block::Block(Code *first, Code *last, CodeSize size) :
+    first(first), last(last), size(size)
 {
-    Block *b;
+    follow = NULL;
+    nfollow = 0;
+}
 
-    b = alloc(Block, 1);
-    b->first = first;
-    b->last = last;
-    b->size = size;
-    b->nfollow = 0;
-
-    return b;
+Block::~Block()
+{
+    if (follow != NULL) {
+	delete[] follow;
+    }
 }
 
 /*
- * NAME:	Block->find()
- * DESCRIPTION:	find a block
+ * find a block
  */
-static Block *block_find(Block *n, CodeSize addr)
+Block *Block::find(CodeSize addr)
 {
-    Block b, *t, *l, *r;
+    Block *n, *t, *l, *r, b(NULL, NULL, 0);
 
+    n = this;
     l = r = &b;
 
     for (;;) {
@@ -42,12 +40,12 @@ static Block *block_find(Block *n, CodeSize addr)
 		break;			/* found */
 	    }
 
-	    t = n->u.t.right;
+	    t = n->t.right;
 	    if (t == NULL) {
 		return NULL;		/* out of range */
 	    }
 	    if (t->first->addr + t->size > addr) {
-		l->u.t.right = n;
+		l->t.right = n;
 		l = n;
 		n = t;
 		if (t->first->addr <= addr) {
@@ -55,46 +53,47 @@ static Block *block_find(Block *n, CodeSize addr)
 		}
 	    } else {
 		/* rotate */
-		n->u.t.right = t->u.t.left;
-		t->u.t.left = n;
-		l->u.t.right = t;
+		n->t.right = t->t.left;
+		t->t.left = n;
+		l->t.right = t;
 		l = t;
-		n = t->u.t.right;
+		n = t->t.right;
 		if (n == NULL) {
 		    return NULL;	/* out of range */
 		}
 	    }
 	} else {
-	    t = n->u.t.left;
+	    t = n->t.left;
 	    if (t->first->addr <= addr) {
-		r->u.t.left = n;
+		r->t.left = n;
 		r = n;
 		n = t;
 	    } else {
 		/* rotate */
-		n->u.t.left = t->u.t.right;
-		t->u.t.right = n;
-		r->u.t.left = t;
+		n->t.left = t->t.right;
+		t->t.right = n;
+		r->t.left = t;
 		r = t;
-		n = t->u.t.left;
+		n = t->t.left;
 	    }
 	}
     }
 
-    l->u.t.right = n->u.t.left;
-    r->u.t.left = n->u.t.right;
-    n->u.t.right = b.u.t.left;
-    n->u.t.left = b.u.t.right;
+    l->t.right = n->t.left;
+    r->t.left = n->t.right;
+    n->t.right = b.t.left;
+    n->t.left = b.t.right;
     return n;
 }
 
 /*
- * NAME:	Block->split()
- * DESCRIPTION:	find and possibly split a block
+ * find and possibly split a block
  */
-static Block *block_split(Block *b, CodeSize addr)
+Block *Block::split(CodeSize addr)
 {
-    b = block_find(b, addr);
+    Block *b;
+
+    b = find(addr);
     if (b->first->addr < addr) {
 	Code *code, *last;
 	CodeSize size;
@@ -112,7 +111,7 @@ static Block *block_split(Block *b, CodeSize addr)
 
 	/* split block */
 	size = last->next->addr - b->first->addr;
-	n = block_new(last->next, b->last, b->size - size);
+	n = new Block(last->next, b->last, b->size - size);
 	n->next = b->next;
 	n->follow = b->follow;
 	n->nfollow = b->nfollow;
@@ -123,9 +122,9 @@ static Block *block_split(Block *b, CodeSize addr)
 	b->nfollow = 0;
 
 	/* insert at root of tree */
-	n->u.t.left = b;
-	n->u.t.right = b->u.t.right;
-	b->u.t.right = NULL;
+	n->t.left = b;
+	n->t.right = b->t.right;
+	b->t.right = NULL;
 	b = n;
     }
 
@@ -133,61 +132,57 @@ static Block *block_split(Block *b, CodeSize addr)
 }
 
 /*
- * NAME:	Block->clear()
- * DESCRIPTION:	clear a list of blocks
+ * delete a list of blocks
  */
-void block_clear(Block *b)
+void Block::clear(Block *b)
 {
     Block *next;
 
     while (b != NULL) {
 	next = b->next;
-	if (b->follow != NULL) {
-	    free(b->follow);
-	}
-	free(b);
+	delete b;
 	b = next;
     }
 }
 
 /*
- * NAME:	Block->function()
- * DESCRIPTION:	break up a function into code blocks
+ * break up a function into code blocks
  */
-Block *block_function(CodeFunction *function)
+Block *Block::create(CodeFunction *function)
 {
     Code *code;
     Block *first, *b, **follow;
     int i;
 
     code = function->list;
-    first = block_new(code, function->last, function->pc);
+    first = new Block(code, function->last, function->pc);
+    first->t.left = first->t.right = NULL;
 
     for (b = first; code != NULL; code = code->next) {
 	switch (code->instruction) {
 	case Code::JUMP:
 	    /* split at jump target */
-	    b = block_split(b, code->u.addr);
+	    b = b->split(code->addr);
 	    if (b == NULL) {
-		block_clear(first);
+		clear(first);
 		return NULL;
 	    }
 	    /* one followup */
-	    follow = alloc(Block*, 1);
+	    follow = new Block*[1];
 	    follow[0] = b;
 
 	    /* start new block */
 	    if (code->next != NULL) {
-		b = block_split(b, code->next->addr);
+		b = b->split(code->next->addr);
 		if (b == NULL) {
-		    free(follow);
-		    block_clear(first);
+		    delete[] follow;
+		    clear(first);
 		    return NULL;
 		}
 	    }
 
 	    /* set followups */
-	    b = block_find(b, code->addr);
+	    b = b->find(code->addr);
 	    b->follow = follow;
 	    b->nfollow = 1;
 	    break;
@@ -196,38 +191,38 @@ Block *block_function(CodeFunction *function)
 	case Code::JUMP_NONZERO:
 	case Code::CATCH:
 	    /* split at jump target */
-	    b = block_split(b, code->u.addr);
+	    b = b->split(code->addr);
 	    if (b == NULL) {
-		block_clear(first);
+		clear(first);
 		return NULL;
 	    }
 	    /* two followups */
-	    follow = alloc(Block*, 2);
+	    follow = new Block*[2];
 	    follow[1] = b;
 
 	    /* start new block */
 	    if (code->next == NULL ||
-		(b=block_split(b, code->next->addr)) == NULL) {
-		free(follow);
-		block_clear(first);
+		(b=b->split(code->next->addr)) == NULL) {
+		delete[] follow;
+		clear(first);
 		return NULL;
 	    }
 	    follow[0] = b;
 
 	    /* set followups */
-	    b = block_find(b, code->addr);
+	    b = b->find(code->addr);
 	    b->follow = follow;
 	    b->nfollow = 2;
 	    break;
 
 	case Code::SWITCH_INT:
 	    /* create jump table with block entry points */
-	    follow = alloc(Block*, code->size);
+	    follow = new Block*[code->size];
 	    for (i = 0; i < code->size; i++) {
-		b = block_split(b, code->u.caseInt[i].addr);
+		b = b->split(code->u.caseInt[i].addr);
 		if (b == NULL) {
-		    free(follow);
-		    block_clear(first);
+		    delete[] follow;
+		    clear(first);
 		    return NULL;
 		}
 		follow[i] = b;
@@ -235,76 +230,76 @@ Block *block_function(CodeFunction *function)
 
 	    /* start new block */
 	    if (code->next != NULL) {
-		b = block_split(b, code->next->addr);
+		b = b->split(code->next->addr);
 		if (b == NULL) {
-		    free(follow);
-		    block_clear(first);
+		    delete[] follow;
+		    clear(first);
 		    return NULL;
 		}
 	    }
 
 	    /* set followups */
-	    b = block_find(b, code->addr);
+	    b = b->find(code->addr);
 	    b->follow = follow;
 	    b->nfollow = code->size;
 	    break;
 
 	case Code::SWITCH_RANGE:
 	    /* create jump table with block entry points */
-	    b = block_find(b, code->addr);
-	    b->follow = follow = alloc(Block*, code->size);
+	    b = b->find(code->addr);
+	    b->follow = follow = new Block*[code->size];
 	    b->nfollow = code->size;
 	    for (i = 0; i < code->size; i++) {
-		follow[i] = b = block_split(b, code->u.caseRange[i].addr);
+		follow[i] = b = b->split(code->u.caseRange[i].addr);
 		if (b == NULL) {
-		    free(follow);
-		    block_clear(first);
+		    delete[] follow;
+		    clear(first);
 		    return NULL;
 		}
 	    }
 
 	    /* start new block */
 	    if (code->next != NULL) {
-		b = block_split(b, code->next->addr);
+		b = b->split(code->next->addr);
 		if (b == NULL) {
-		    free(follow);
-		    block_clear(first);
+		    delete[] follow;
+		    clear(first);
 		    return NULL;
 		}
 	    }
 
 	    /* set followups */
-	    b = block_find(b, code->addr);
+	    b = b->find(code->addr);
 	    b->follow = follow;
 	    b->nfollow = code->size;
 	    break;
 
 	case Code::SWITCH_STRING:
 	    /* create jump table with block entry points */
-	    b = block_find(b, code->addr);
-	    b->follow = follow = alloc(Block*, code->size);
+	    b = b->find(code->addr);
+	    b->follow = follow = new Block*[code->size];
 	    b->nfollow = code->size;
 	    for (i = 0; i < code->size; i++) {
-		follow[i] = b = block_split(b, code->u.caseString[i].addr);
+		follow[i] = b = b->split(code->u.caseString[i].addr);
 		if (b == NULL) {
-		    free(follow);
-		    block_clear(first);
+		    delete[] follow;
+		    clear(first);
 		    return NULL;
 		}
 	    }
 
 	    /* start new block */
 	    if (code->next != NULL) {
-		b = block_split(b, code->next->addr);
+		b = b->split(code->next->addr);
 		if (b == NULL) {
-		    free(follow);
-		    block_clear(first);
+		    delete[] follow;
+		    clear(first);
 		    return NULL;
 		}
 	    }
 
 	    /* set followups */
-	    b = block_find(b, code->addr);
+	    b = b->find(code->addr);
 	    b->follow = follow;
 	    b->nfollow = code->size;
 	    break;
--- jit/block.h
+++ jit/block.h
@@ -1,17 +1,28 @@
-typedef struct Block {
+class Block {
+public:
+    Block(Code *first, Code *last, CodeSize size);
+    ~Block();
+
+    static Block *create(CodeFunction *func);
+
+    Code *first, *last;			/* first and last code in block */
+    Block *next;			/* next block */
+    Block **follow;			/* following blocks */
+    uint16_t nfollow;			/* # following blocks */
+    CodeSize size;			/* size of block */
+
+private:
+    Block *find(CodeSize addr);
+    Block *split(CodeSize addr);
+
+    static void clear(Block *b);
+
     union {
 	struct {
-	    struct Block *left, *right;	/* left and right child in tree */
+	    Block *left, *right;	/* left and right child in tree */
 	} t;
 	struct {
 	    struct FlowData *flow;	/* flow data */
 	} d;
-    } u;
-    Code *first, *last;			/* first and last code in block */
-    struct Block *next;			/* next block */
-    struct Block **follow;		/* following blocks */
-    uint16_t nfollow;			/* # following blocks */
-    CodeSize size;			/* size of block */
-} Block;
-
-extern Block *block_function	(CodeFunction*);
+    };
+};
