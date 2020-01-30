d38885cb1d4641b73606ce98e01a788d63a38b89
Felix A. Croes <felix@dworkin.nl>
2020-01-30 21:32:05 +0100

    Support hash digests SHA256 and SHA512.
    And override MD5 and SHA1.

--- Makefile
+++ Makefile
@@ -21,6 +21,8 @@ jit:	jit.$(EXT)
 
 zlib:	zlib.$(EXT)
 
+crypto:	crypto.$(EXT)
+
 src/lpc_ext.o:	src/lpc_ext.c src/lpc_ext.h
 	$(CC) -o $@ -c $(CFLAGS) -Isrc src/lpc_ext.c
 
@@ -28,6 +30,15 @@ kfun/lower_case.o:	kfun/lower_case.c src/lpc_ext.h
 	$(CC) -o $@ -c $(CFLAGS) -Isrc kfun/lower_case.c
 
 lower_case.$(EXT):	kfun/lower_case.o $(OBJ)
+	$(LD) -o $@ $(LDFLAGS) $+
+
+kfun/rgx/regexp.o:	kfun/rgx/regexp.c src/lpc_ext.h
+	$(CC) -o $@ -c $(CFLAGS) -Isrc -I$(LIBLIB) kfun/rgx/regexp.c
+
+kfun/rgx/regex.o:	kfun/rgx/libiberty/regex.c
+	$(CC) -o $@ -c $(CFLAGS) -I$(LIBLIB) `cat $(LIBLIB)/config` $+
+
+regexp.$(EXT):		kfun/rgx/regexp.o kfun/rgx/regex.o $(OBJ)
 	$(LD) -o $@ $(LDFLAGS) $+
 
 jit/jitcomp::
@@ -39,15 +50,6 @@ jit/jit.o::
 jit.$(EXT):	jit/jit.o $(OBJ) jit/jitcomp
 	$(LD) -o $@ $(LDFLAGS) jit/jit.o $(OBJ)
 
-kfun/rgx/regexp.o:	kfun/rgx/regexp.c src/lpc_ext.h
-	$(CC) -o $@ -c $(CFLAGS) -Isrc -I$(LIBLIB) kfun/rgx/regexp.c
-
-kfun/rgx/regex.o:	kfun/rgx/libiberty/regex.c
-	$(CC) -o $@ -c $(CFLAGS) -I$(LIBLIB) `cat $(LIBLIB)/config` $+
-
-regexp.$(EXT):		kfun/rgx/regexp.o kfun/rgx/regex.o $(OBJ)
-	$(LD) -o $@ $(LDFLAGS) $+
-
 kfun/zlib/zlib.o:	kfun/zlib/zlib.c src/lpc_ext.h
 	$(CC) -o $@ -c $(CFLAGS) -Isrc -Ikfun/zlib/$(ZLIB) -DVERSION=$(ZLIB) \
 	      kfun/zlib/zlib.c
@@ -55,7 +57,14 @@ kfun/zlib/zlib.o:	kfun/zlib/zlib.c src/lpc_ext.h
 zlib.$(EXT):	kfun/zlib/zlib.o $(OBJ)
 	$(LD) -o $@ $(LDFLAGS) $+ -lz
 
+kfun/crypto.o:	kfun/crypto.c src/lpc_ext.h
+	$(CC) -o $@ -c $(CFLAGS) -Isrc kfun/crypto.c
+
+crypto.$(EXT):	kfun/crypto.o $(OBJ)
+	$(LD) -o $@ $(LDFLAGS) $+ -lcrypto
+
 clean:
-	rm -f lower_case.$(EXT) regexp.$(EXT) zlib.$(EXT) jit.$(EXT) src/*.o \
-	      kfun/*.o kfun/rgx/*.o kfun/zlib/*.o
+	rm -f lower_case.$(EXT) regexp.$(EXT) zlib.$(EXT) jit.$(EXT) \
+	      crypto.$(EXT) src/*.o kfun/*.o kfun/rgx/*.o kfun/zlib/*.o \
+	      kfun/crypto/*.o
 	$(MAKE) -C jit clean
---
+++ kfun/crypto.c
@@ -0,0 +1,112 @@
+/*
+ * crypto kfun extensions
+ * This code is released into the public domain.
+ */
+
+# include <openssl/evp.h>
+# include "lpc_ext.h"
+
+static const EVP_MD *md_md5, *md_sha1, *md_sha256, *md_sha512;
+
+static void hash(const EVP_MD *md, LPC_frame f, int nargs, LPC_value retval)
+{
+    EVP_MD_CTX *ctx;
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    int i, length;
+    LPC_string str;
+
+    ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, md, NULL);
+
+    for (i = 0; i < nargs; i++) {
+	str = lpc_string_getval(lpc_frame_arg(f, nargs, i));
+	EVP_DigestUpdate(ctx, lpc_string_text(str), lpc_string_length(str));
+    }
+
+    EVP_DigestFinal_ex(ctx, digest, &length);
+    EVP_MD_CTX_free(ctx);
+
+    /* make a digest string */
+    str = lpc_string_new(lpc_frame_dataspace(f), digest, length);
+
+    /* put result in return value */
+    lpc_string_putval(retval, str);
+}
+
+static void md5(LPC_frame f, int nargs, LPC_value retval)
+{
+    int i;
+    LPC_string str;
+
+    lpc_runtime_ticks(f, 3 * nargs + 64);
+    for (i = 0; i < nargs; i++) {
+	str = lpc_string_getval(lpc_frame_arg(f, nargs, i));
+	lpc_runtime_ticks(f, lpc_string_length(str));
+    }
+    hash(md_md5, f, nargs, retval);
+}
+
+static void sha1(LPC_frame f, int nargs, LPC_value retval)
+{
+    int i;
+    LPC_string str;
+
+    lpc_runtime_ticks(f, 3 * nargs + 64);
+    for (i = 0; i < nargs; i++) {
+	str = lpc_string_getval(lpc_frame_arg(f, nargs, i));
+	lpc_runtime_ticks(f, lpc_string_length(str));
+    }
+    hash(md_sha1, f, nargs, retval);
+}
+
+static void sha256(LPC_frame f, int nargs, LPC_value retval)
+{
+    int i;
+    LPC_string str;
+
+    lpc_runtime_ticks(f, 3 * nargs + 64);
+    for (i = 0; i < nargs; i++) {
+	str = lpc_string_getval(lpc_frame_arg(f, nargs, i));
+	lpc_runtime_ticks(f, lpc_string_length(str));
+    }
+    hash(md_sha256, f, nargs, retval);
+}
+
+static void sha512(LPC_frame f, int nargs, LPC_value retval)
+{
+    int i;
+    LPC_string str;
+
+    lpc_runtime_ticks(f, 3 * nargs + 64);
+    for (i = 0; i < nargs; i++) {
+	str = lpc_string_getval(lpc_frame_arg(f, nargs, i));
+	lpc_runtime_ticks(f, lpc_string_length(str));
+    }
+    hash(md_sha512, f, nargs, retval);
+}
+
+static char md5_proto[] = { LPC_TYPE_STRING, LPC_TYPE_STRING, LPC_TYPE_ELLIPSIS,
+			    0 };
+static char sha1_proto[] = { LPC_TYPE_STRING, LPC_TYPE_STRING,
+			     LPC_TYPE_ELLIPSIS, 0 };
+static char sha256_proto[] = { LPC_TYPE_STRING, LPC_TYPE_STRING,
+			       LPC_TYPE_ELLIPSIS, 0 };
+static char sha512_proto[] = { LPC_TYPE_STRING, LPC_TYPE_STRING,
+			       LPC_TYPE_ELLIPSIS, 0 };
+static LPC_ext_kfun kf[] = {
+    { "hash MD5", md5_proto, &md5 },
+    { "hash SHA1", sha1_proto, &sha1 },
+    { "hash SHA256", sha256_proto, &sha256 },
+    { "hash SHA512", sha512_proto, &sha512 }
+};
+
+int lpc_ext_init(int major, int minor, const char *config)
+{
+    md_md5 = EVP_get_digestbyname("MD5");
+    md_sha1 = EVP_get_digestbyname("SHA1");
+    md_sha256 = EVP_get_digestbyname("SHA256");
+    md_sha512 = EVP_get_digestbyname("SHA512");
+
+    lpc_ext_kfun(kf, 4);
+    return 1;
+}
