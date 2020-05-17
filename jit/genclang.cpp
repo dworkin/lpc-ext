db3db602f6b6041642919f01408829d5ecccf5ab
Felix A. Croes <felix@dworkin.nl>
2020-05-17 13:49:31 +0200

    Let JIT compiler optimize for native platform.

--- jit/genclang.cpp
+++ jit/genclang.cpp
@@ -2410,7 +2410,7 @@ bool ClangObject::emit(char *base)
 # else
 	    "\"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Tools\\Llvm\\bin\\clang.exe\"\""
 # endif
-	    " -Os -shared"
+	    " -march=native -Os -shared"
 # if defined(__APPLE__) || defined(WIN32)
 	    " -Wno-override-module"
 # endif
